3025e219c9a2a67c4216690a7357211466dd07f1
jeffreykira <jeffrey77918@gmail.com>
2020-06-22 17:59:16 +0800

    add unit test (#2)

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # url-shortener
 
-## Environment dependency
-1. python3
+## Environment Dependency
+- python3
+- pip install -r requirements.txt
+
+## Run Unit Test
+- test all files
+  `project_path > python -m unittest discover -v url_shortener/test`
+
+- test specific file
+  `project_path > python -m unittest -v url_shortener/test/{test_file_name}`
--- url_shortener/domain/shorten.py
+++ url_shortener/domain/shorten.py
@@ -1,5 +1,6 @@
 import logging
 import hashlib
+import requests
 from url_shortener import utils
 from url_shortener import app_config
 from url_shortener.exception import *
@@ -8,13 +9,20 @@ log = logging.getLogger(__name__)
 
 
 def _shorten_code(url):
+    if not isinstance(url, str):
+        raise DataValidationError()
+
     return hashlib.md5(url.encode('utf-8')).hexdigest()[-5:]
 
 
 @utils.log_scope(log)
 def create(url):
-    if not isinstance(url, str):
-        raise DataValidationError()
+    if not url.lower().startswith(('http://', 'https://')):
+        raise DataValidationError('url schema error')
+
+    req = requests.get(url)
+    if req.status_code != 200:
+        raise ResourceNotFound('URL does not exist')
 
     short_code = _shorten_code(url)
     short_url = app_config.CONFIG.URL_PREFIX + short_code
